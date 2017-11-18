0ac757c6900252f41b2fe0fe0be3fd4677b77a8c
Ken Sodemann <ken@ionic.io>
2017-11-18 09:51:52 -0600

    feat(e2e): add e2e tests (#4)

--- test/web/unit/specs/pages/links.spec.js
+++ test/web/unit/specs/pages/links.spec.js
@@ -5,7 +5,7 @@ describe('links.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(Page);
     const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    expect(vm.$el.querySelector('.links h1').textContent)
       .to.equal('Welcome to Your Vue.js App');
   });
 });
