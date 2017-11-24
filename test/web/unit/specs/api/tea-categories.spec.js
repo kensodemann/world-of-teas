718394ac3385ad264737d8f1566f14e901985e83
Ken Sodemann <ken@ionic.io>
2017-11-24 07:36:45 -0600

    feat(categories): display the existing categories (#7)

--- test/web/unit/specs/api/tea-categories.spec.js
+++ test/web/unit/specs/api/tea-categories.spec.js
@@ -3,11 +3,11 @@ import VueResource from 'vue-resource';
 import VueResourceMock from 'vue-resource-mock';
 import teaCategories from '@/api/tea-categories';
 
-import mockDataService from '../../../../data/mock-data-service';
-import mockTeaCategories from '../../../../data/mock-tea-categories';
+import testDataService from '@/assets/test-data/data-service';
+import testTeaCategories from '@/assets/test-data/tea-categories';
 
 Vue.use(VueResource);
-Vue.use(VueResourceMock, mockDataService);
+Vue.use(VueResourceMock, testDataService);
 
 describe('tea-categories', function() {
   it('exists', function() {
@@ -17,7 +17,7 @@ describe('tea-categories', function() {
   describe('get all', function() {
     it('gets all of the tea categories', function() {
       return teaCategories.getAll().then(res => {
-        expect(res).to.deep.equal(mockTeaCategories);
+        expect(res).to.deep.equal(testTeaCategories);
       });
     });
   });
--- test/web/unit/specs/pages/browse-by-category.spec.js
+++ test/web/unit/specs/pages/browse-by-category.spec.js
@@ -1,11 +1,44 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
+import VueResourceMock from 'vue-resource-mock';
 import Page from '@/components/pages/browse-by-category';
 
+import testDataService from '@/assets/test-data/data-service';
+import testTeaCategories from '@/assets/test-data/tea-categories';
+
+Vue.use(VueResource);
+Vue.use(VueResourceMock, testDataService);
+
 describe('browse-by-category.vue', () => {
-  it('should render correct contents', () => {
+  let vm;
+  beforeEach(() => {
     const Constructor = Vue.extend(Page);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.browse-by-category h1').textContent)
-      .to.equal('I Like to Browse by Category');
+    vm = new Constructor().$mount();
+  });
+
+  it('should render correct title', () => {
+    expect(vm.$el.querySelector('.browse-by-category h1').textContent).to.equal(
+      'I Like to Browse by Category'
+    );
+  });
+
+  it('should fetch the tea categories', () => {
+    return Vue.nextTick().then(() => {
+      expect(vm.categories).to.deep.equal(testTeaCategories);
+    });
+  });
+
+  it('should render a title for each category', () => {
+    return Vue.nextTick().then(() => {
+      return Vue.nextTick().then(() => {
+        const titles = vm.$el.querySelectorAll(
+          '.browse-by-category .category-title'
+        );
+        expect(titles.length).to.equal(vm.categories.length);
+        for (let i = 0; i < titles.length; i++) {
+          expect(titles[i].textContent).to.equal(vm.categories[i].name);
+        }
+      });
+    });
   });
 });
