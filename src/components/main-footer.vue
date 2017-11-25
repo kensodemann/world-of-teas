f609d980903839ba23a7d8739c8da2303fdc4d61
Ken Sodemann <ken@ionic.io>
2017-11-25 07:45:26 -0600

    feat(footer): include version information

--- src/components/main-footer.vue
+++ src/components/main-footer.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="footer">
-    <div class="footer-title">World of Teas</div>
-    <div class="footer-copyright">Copyright &copy; 2017 - Kenneth W. Sodemann</div>
+    <div class="footer-title">World of Teas - v{{version}} ({{name}})</div>
+    <div class="footer-copyright">Copyright &copy; {{year}} - Kenneth W. Sodemann</div>
   </div>
 </template>
 
 <script>
-export default {};
+import moment from 'moment';
+import version from '../assets/version.json';
+
+export default {
+  data: function() {
+    return {
+      version: version.version,
+      name: version.name,
+      year: moment(version.date).format('YYYY')
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
--- test/web/unit/specs/main-footer.spec.js
+++ test/web/unit/specs/main-footer.spec.js
@@ -1,18 +1,20 @@
 import Vue from 'vue';
 import Component from '@/components/main-footer';
+import moment from 'moment';
+import version from '@/assets/version.json';
 
 describe('main-footer.vue', () => {
   it('should render the title line correctly', () => {
     const Constructor = Vue.extend(Component);
     const vm = new Constructor().$mount();
     const text = vm.$el.querySelector('.footer-title').textContent;
-    expect(text).to.equal('World of Teas');
+    expect(text).to.equal(`World of Teas - v${version.version} (${version.name})`);
   });
 
   it('should render the copyright line correctly', () => {
     const Constructor = Vue.extend(Component);
     const vm = new Constructor().$mount();
     const text = vm.$el.querySelector('.footer-copyright').textContent;
-    expect(text).to.equal('Copyright © 2017 - Kenneth W. Sodemann');
+    expect(text).to.equal(`Copyright © ${moment(version.date).format('YYYY')} - Kenneth W. Sodemann`);
   });
 });
