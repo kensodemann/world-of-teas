31f09ec532072724c8dbd9caf67f5e849d45a8c1
Ken Sodemann <ken@ionic.io>
2017-11-25 06:43:55 -0600

    Code Review: fix tests

--- test/web/unit/specs/main-footer.spec.js
+++ test/web/unit/specs/main-footer.spec.js
@@ -2,9 +2,17 @@ import Vue from 'vue';
 import Component from '@/components/main-footer';
 
 describe('main-footer.vue', () => {
-  it('should render correct contents', () => {
+  it('should render the title line correctly', () => {
     const Constructor = Vue.extend(Component);
     const vm = new Constructor().$mount();
-    expect(vm.$el.textContent).to.equal('This is the footer');
+    const text = vm.$el.querySelector('.footer-title').textContent;
+    expect(text).to.equal('World of Teas');
+  });
+
+  it('should render the copyright line correctly', () => {
+    const Constructor = Vue.extend(Component);
+    const vm = new Constructor().$mount();
+    const text = vm.$el.querySelector('.footer-copyright').textContent;
+    expect(text).to.equal('Copyright © 2017 - Kenneth W. Sodemann');
   });
 });
