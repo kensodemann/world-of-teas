36bf2d92eaf6ae85bc4eb8dbcf507a287041286f
Ken Sodemann <ken@ionic.io>
2017-12-04 05:36:52 -0600

    feat(authentication): add  password checking routine

--- server/services/users.js
+++ server/services/users.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const encryption = require('./encryption');
+
 module.exports = class Users {
   constructor(pool) {
     this._pool = pool;
@@ -15,9 +17,17 @@ module.exports = class Users {
   async get(id) {
     const client = await this._pool.connect();
     const data = await (/.*@.*/.test(id)
-      ? client.query('select * from users where email = $1', [id])
+      ? client.query('select * from users where upper(email) = upper($1)', [id])
       : client.query('select * from users where id = $1', [id]));
     client.release();
     return data.rows && data.rows[0];
+  }
+
+  async passwordMatches(id, password) {
+    const client = await this._pool.connect();
+    const data = await client.query('select * from user_credentials where id = $1', [id]);
+    const cred = data.rows && data.rows[0];
+    client.release();
+    return !!(cred && cred.password === encryption.hash(cred.salt, password));
   }
 };
