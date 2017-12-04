610247efc4f56d756877c7908fe9077bc8b38ffc
Ken Sodemann <ken@ionic.io>
2017-12-04 08:57:55 -0600

    feat(authentication): add  password checking routine (#49)

--- test/server/services/users.spec.js
+++ test/server/services/users.spec.js
@@ -2,12 +2,12 @@
 
 const expect = require('chai').expect;
 const MockPool = require('../mocks/mock-pool');
+const proxyquire = require('proxyquire');
 const sinon = require('sinon');
 const Servce = require('../../../server/services/users');
 
 describe('service: users', function() {
   let pool;
-  let service;
   let testData;
 
   beforeEach(function() {
@@ -26,10 +26,15 @@ describe('service: users', function() {
         email: 'another.fake.email@aol.com'
       }
     ];
-    service = new Servce(pool);
   });
 
   describe('getAll', function() {
+    let service;
+
+    beforeEach(function() {
+      service = new Servce(pool);
+    });
+
     it('connects to the pool', function() {
       sinon.spy(pool, 'connect');
       service.getAll();
@@ -44,7 +49,7 @@ describe('service: users', function() {
         .true;
     });
 
-    it('returns the data', async function() {
+    it('resolves the data', async function() {
       sinon.stub(pool.test_client, 'query');
       pool.test_client.query.returns(Promise.resolve({ rows: testData }));
       const data = await service.getAll();
@@ -59,6 +64,12 @@ describe('service: users', function() {
   });
 
   describe('get', function() {
+    let service;
+
+    beforeEach(function() {
+      service = new Servce(pool);
+    });
+
     it('connects to the pool', function() {
       sinon.spy(pool, 'connect');
       service.get(42);
@@ -92,13 +103,14 @@ describe('service: users', function() {
       await service.get('42@1138.73');
       expect(pool.test_client.query.calledOnce).to.be.true;
       expect(
-        pool.test_client.query.calledWith('select * from users where email = $1', [
-          '42@1138.73'
-        ])
+        pool.test_client.query.calledWith(
+          'select * from users where upper(email) = upper($1)',
+          ['42@1138.73']
+        )
       ).to.be.true;
     });
 
-    it('returns the data', async function() {
+    it('resolves the data', async function() {
       sinon.stub(pool.test_client, 'query');
       pool.test_client.query.returns(
         Promise.resolve({
@@ -121,7 +133,7 @@ describe('service: users', function() {
       });
     });
 
-    it('returns undefined if not found', async function() {
+    it('resolves undefined if not found', async function() {
       sinon.stub(pool.test_client, 'query');
       pool.test_client.query.returns(Promise.resolve({ rows: [] }));
       const data = await service.get(42);
@@ -131,6 +143,76 @@ describe('service: users', function() {
     it('releases the client', async function() {
       sinon.spy(pool.test_client, 'release');
       await service.get(42);
+      expect(pool.test_client.release.calledOnce).to.be.true;
+    });
+  });
+
+  describe('password is valid', function() {
+    let service;
+
+    beforeEach(function() {
+      const mockEncryption = {
+        hash: sinon.stub()
+      };
+      mockEncryption.hash
+        .withArgs('39949AAC43', 'IamPassw0rd')
+        .returns('F00BA4');
+      const ProxiedService = proxyquire('../../../server/services/users', {
+        './encryption': mockEncryption
+      });
+      service = new ProxiedService(pool);
+    });
+
+    it('connects to the pool', function() {
+      sinon.spy(pool, 'connect');
+      service.passwordMatches(42, 'shhhhhIam$ecret');
+      expect(pool.connect.calledOnce).to.be.true;
+    });
+
+    it('queries the user credentials', async function() {
+      sinon.spy(pool.test_client, 'query');
+      await service.passwordMatches(42, 'IamPassw0rd');
+      expect(pool.test_client.query.calledOnce).to.be.true;
+      expect(
+        pool.test_client.query.calledWith(
+          'select * from user_credentials where id = $1',
+          [42]
+        )
+      ).to.be.true;
+    });
+
+    it('resolves false if the user has no credentials record', async function() {
+      sinon.stub(pool.test_client, 'query');
+      pool.test_client.query.returns(Promise.resolve({ rows: [] }));
+      const match = await service.passwordMatches(42, 'IamPassw0rd');
+      expect(match).to.be.false;
+    });
+
+    it('resolves false if the password hashes do not match', async function() {
+      sinon.stub(pool.test_client, 'query');
+      pool.test_client.query.returns(
+        Promise.resolve({
+          rows: [{ id: 42, salt: '39949AAC43', password: 'ABBA1357CDDF' }]
+        })
+      );
+      const match = await service.passwordMatches(42, 'IamPassw0rd');
+      expect(match).to.be.false;
+    });
+
+    it('resolves true if the password hashes do match', async function() {
+      sinon.stub(pool.test_client, 'query');
+      pool.test_client.query.returns(
+        Promise.resolve({
+          rows: [{ id: 42, salt: '39949AAC43', password: 'F00BA4' }]
+        })
+      );
+      const match = await service.passwordMatches(42, 'IamPassw0rd');
+      expect(match).to.be.true;
+    });
+
+    it('releases the client', async function() {
+      sinon.spy(pool.test_client, 'release');
+      await service.passwordMatches(73, 'coop3r');
       expect(pool.test_client.release.calledOnce).to.be.true;
     });
   });
