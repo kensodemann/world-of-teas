1a68057556b1b5d167c76a49e7262dec809c0025
Ken Sodemann <ken@ionic.io>
2018-01-25 10:26:57 -0800

    feature(rating): add a rating indicator component

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
     <div class="page-title">Find Great Tea</div>
-    <b-form inline>
-      <b-form-group id="searchInputGroup">
-        <b-form-input id="searchInput"
-                      name="searchInput"
-                      type="text"
-                      v-model="search"
-                      placeholder="Search for Tea">
-        </b-form-input>
-      </b-form-group>
-      <b-button variant="primary" v-b-modal.teaEditor>+</b-button>
+    <b-form>
+      <div class="d-flex">
+        <b-form-group id="searchInputGroup" class="mb-0">
+          <b-form-input id="searchInput"
+                        name="searchInput"
+                        type="text"
+                        v-model="search"
+                        placeholder="Search for Tea">
+          </b-form-input>
+        </b-form-group>
+        <b-button variant="primary" v-b-modal.teaEditor>+</b-button>
+      </div>
     </b-form>
 
     <div class="list tea-list">
@@ -29,10 +31,6 @@
   flex-grow: 1;
   margin-right: 10px;
 }
-
-#searchInput {
-  width: 100%;
-}
 </style>
 
 
@@ -45,6 +43,7 @@ export default {
   },
   data() {
     return {
+      stars: 3,
       search: ''
     };
   }
