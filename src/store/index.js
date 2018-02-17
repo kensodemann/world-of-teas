d05956930ef8887d2ed8b53ab2a2e15d5cab9270
Ken Sodemann <ken@ionic.io>
2018-02-17 16:43:57 -0600

    feat(tea): add teas to the store (#97)
    Fixes #90

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import identity from './modules/identity';
+import teas from './modules/teas';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
   modules: {
-    identity
+    identity,
+    teas
   }
 });
---
+++ src/store/modules/teas/mutations.js
@@ -0,0 +1,39 @@
+export default {
+  load: (state, data) => {
+    state.list = data;
+    state.hash = hash(data);
+  },
+
+  remove: (state, data) => {
+    const { id } = data;
+    if (id) {
+      const idx = state.list.findIndex(element => element.id === id);
+      if (idx > -1) {
+        state.list.splice(idx, 1);
+      }
+      delete state.hash[id];
+    }
+  },
+
+  save: (state, data) => {
+    const { id } = data;
+    if (id) {
+      const idx = state.list.findIndex(element => element.id === id);
+      if (idx > -1) {
+        state.list.splice(idx, 1, data);
+        state.hash[id] = data;
+      } else {
+        state.list.push(data);
+        state.hash[id] = data;
+      }
+    }
+  }
+};
+
+function hash(data) {
+  let obj = {};
+  data.forEach(element => {
+    obj[element.id] = element;
+  });
+  return obj;
+}
