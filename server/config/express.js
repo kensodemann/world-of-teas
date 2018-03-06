8d1212dfcafcbda36f08f861b922ccce8e6b6f9c
Ken Sodemann <ken@ionic.io>
2018-03-06 17:15:23 -0600

    feat(security): experiment with restrictive ACAO

--- server/config/express.js
+++ server/config/express.js
@@ -10,7 +10,7 @@ module.exports = app => {
   app.use(bodyParser.json());
 
   app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://localhost:8080');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
