d1f9926614133b8908b33f428223ea486841f27c
Ken Sodemann <ken@ionic.io>
2018-07-08 05:33:42 -0500

    feat(categories): fill out the tea categories (#105)

--- src/components/pages/browse-by-category.vue
+++ src/components/pages/browse-by-category.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="browse-by-category">
-    <div class="page-title">Types of Tea</div>
-    <div>A list of categories will eventually be here</div>
-    <div class="category-title" v-for="category in categories" :key="category.id">{{category.name}}</div>
+  <div class="page">
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col col-xl-3 col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center"  v-for="category in categories" :key="category.id">
+          <tea-category :category=category></tea-category>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<style scoped>
+.col {
+  margin-top: 1em;
+}
+</style>
+
+
 <script>
+import TeaCategory from '../tea-category';
 export default {
+  components: {
+    TeaCategory
+  },
   computed: {
     categories() {
       return this.$store.state.teaCategories.list;
